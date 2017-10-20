cc1e3938a053ed1b05d4ec505573e42ea02b4834
johnypony3 <edward@theemm.com>
2017-10-20 05:24:03 -0700

    elastic ip is giving me a hard time, not necessary

--- terraform/instances.tf
+++ terraform/instances.tf
@@ -39,8 +39,8 @@ resource "null_resource" "docker_deploy" {
     inline = [
       "echo remote-exec docker_deploy",
       "docker login -u ${var.DOCKER_HUB_USER} -p ${var.DOCKER_HUB_PASS}",
-      "export DOCKER_COMPOSE_FILE=$DOCKER_COMPOSE_FILE",
-      "bash ./docker/provision-files/docker-compose-start.sh",
+      "docker-compose -f './docker/compose-files/${var.DOCKER_COMPOSE_FILE}' down"
+      "docker-compose -f './docker/compose-files/${var.DOCKER_COMPOSE_FILE}' up --build -d"
       "sudo rm -rf ./docker",
     ]
   }
